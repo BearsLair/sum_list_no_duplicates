fd2d6ef5941861fa5a8f1afeb19d5a30fbd1d02d
Patrick Gross <bears_lair@protonmail.com>
2024-07-01 12:44:00 -0700

    initial commit with tests

---
+++ sum_list_no_duplicates.js
@@ -0,0 +1,14 @@
+function sumNoDuplicates(numList) {
+  return 0;
+}
+
+// Test cases
+console.log(sumNoDuplicates([1, 1, 2, 2, 3])); // 3
+console.log(sumNoDuplicates([0, 1, 4, 4, 0, 4, 2, 5, 9, 0, 10, 9, 0, 1, 2])); // 15
+console.log(
+  sumNoDuplicates([10, 10, 1, 4, 10, 3, 9, 6, 10, 10, 9, 9, 10, 5, 6, 10, 4, 1])
+); // 8
+console.log(
+  sumNoDuplicates([4, 2, 10, 9, 10, 5, 1, 7, 1, 9, 8, 7, 4, 2, 5, 8, 3, 10, 8])
+); // 3
+console.log(sumNoDuplicates([])); // 0
